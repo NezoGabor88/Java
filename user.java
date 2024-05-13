aa6d3a8474beb8fa59071774c37d5169a8b3be5d
Gábor Néző <39116198+NezoGabor88@users.noreply.github.com>
2024-05-13 23:01:32 +0200

    Create user.java

---
+++ user.java
@@ -0,0 +1,17 @@
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    // Getters and setters (omitted for brevity)
+}
