8c58a43ccf6e6089f469d79cb92ddec0a7fa8f46
Gábor Néző <39116198+NezoGabor88@users.noreply.github.com>
2024-05-13 22:58:24 +0200

    Update bike.java

--- bike.java
+++ bike.java
@@ -23,6 +23,9 @@ public class Bike {
         return gear;
     }
 
+
+    }
+
     public void setGear(int gear) {
         this.gear = gear;
     }
@@ -35,10 +38,28 @@ public class Bike {
         this.price = price;
     }
 
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
     // Method to display bike information
     public void displayInfo() {
         System.out.println("Brand: " + brand);
+        System.out.println("Type: " + type);
         System.out.println("Number of gears: " + gear);
+        System.out.println("Year: " + year);
         System.out.println("Price: $" + price);
     }
 }
